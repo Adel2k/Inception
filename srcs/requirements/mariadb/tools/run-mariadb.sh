b2a8d7e3ba1412fe44afa701fa79346f9f08660a
Adeline <adelineeminian13@gmail.com>
2025-02-06 17:56:58 +0400

    mandatory done

--- srcs/requirements/mariadb/tools/run-mariadb.sh
+++ srcs/requirements/mariadb/tools/run-mariadb.sh
@@ -1,39 +1,32 @@
 #!/bin/bash
 
-# Ensure necessary directories exist and have correct permissions
 mkdir -p /var/run/mysqld
 chown -R mysql:mysql /var/lib/mysql
-chmod -R 755 /var/lib/mysql
+chmod -R 755 /var/lib/mysql\
 
-# Initialize the database if not already initialized
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
 fi
 
-# Start MariaDB in the background
 mysqld_safe --datadir=/var/lib/mysql &
 
-# Wait for MariaDB to be fully ready (this step is important)
 until mysqladmin ping --silent; do
     echo "Waiting for MariaDB to start..."
     sleep 2
 done
 
-echo "MariaDB started successfully!"
 
-# Create database and user through SQL commands
 cat <<EOF > db.sql
 CREATE DATABASE IF NOT EXISTS $DB_NAME;
 CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
 GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
 FLUSH PRIVILEGES;
+UPDATE wp_options SET option_value = 'http://localhost' WHERE option_name = 'siteurl';
+UPDATE wp_options SET option_value = 'http://localhost' WHERE option_name = 'home';
 EOF
 
-# Execute SQL commands to create DB and user
 mysql < db.sql
 
-# Clean up: Kill the background mysqld_safe process and start MariaDB in the foreground
 kill $(cat /var/run/mysqld/mysqld.pid)
 
-# Start MariaDB in the foreground (this keeps the container running)
 exec mysqld --datadir=/var/lib/mysql
